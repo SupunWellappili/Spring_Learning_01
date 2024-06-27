9c7a5399416fd12af40b3608813ea8fa9aabe8d9
SupunWellappili <supunchandana1814@gmail.com>
2024-06-28 02:02:19 +0530

    basePackageClasses

--- src/config/AppConfig.java
+++ src/config/AppConfig.java
@@ -1,20 +1,26 @@
 package config;
 
 import bean.MyConnection;
+import bean.SpringBeanOne;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 
+//How to scan POJOs and enter to the Application Context
 @Configuration
-@ComponentScan(basePackages = "bean")
+//@ComponentScan(basePackages = "bean")
+@ComponentScan(basePackageClasses = SpringBeanOne.class)
 public class AppConfig {
 
     public AppConfig() {
         System.out.println("App Config Object Created");
     }
 
+    //When we are not allowed to put the @Component annotation we can use this method to
+    //Introduce a spring bean to the application context
     @Bean
-    public MyConnection getConnecting(){
+    public MyConnection getConnecting() {
         return new MyConnection();
     }
 }
