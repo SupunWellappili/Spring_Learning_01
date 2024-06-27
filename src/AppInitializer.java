5bd18f92bef7b95a60567426349678ccd1a32f4f
SupunWellappili <supunchandana1814@gmail.com>
2024-06-27 23:59:50 +0530

    Hooking Process

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -8,6 +8,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 //POJO driven approach / Annotation Driven
 public class AppInitializer {
     public static void main(String[] args) {
+
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("JVM is about to shut down");
+            }
+        }));
+
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
         ctx.register(AppConfig.class);
         ctx.refresh();
