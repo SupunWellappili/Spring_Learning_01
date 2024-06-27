71801f9ac4f20098e30dbe7134fa523fb8b5f82c
SupunWellappili <supunchandana1814@gmail.com>
2024-06-27 19:58:07 +0530

    Introduced another bean

---
+++ src/bean/beanTwo.java
@@ -0,0 +1,10 @@
+package bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class beanTwo {
+    public beanTwo() {
+        System.out.println("Object Created from bean two");
+    }
+}
